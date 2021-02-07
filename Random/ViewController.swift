6562ad3dfc023e43b143c7865b0f2e64171ffb54
Sosin Vitalii <375693@mail.ru>
2021-02-07 23:30:29 +0300

    Initial Commit

---
+++ Random/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random
+//
+//  Created by Vitalii Sosin on 07.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

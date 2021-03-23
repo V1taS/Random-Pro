5336a588be5fce68ed3b0f901b3897594241f064
Sosin Vitalii <375693@mail.ru>
2021-03-23 23:10:09 +0300

    codereview

--- Random/UI/Screens/Music/MusicView.swift
+++ Random/UI/Screens/Music/MusicView.swift
@@ -55,55 +55,15 @@ struct MusicView: View {
             
             .sheet(isPresented: appBinding.music.showSettings,
                    onDismiss: {
-                    if appBinding.music.listMusic.wrappedValue.isEmpty {
-                        getMusicFile(state: appBinding)
-                        musicPlayer.pause()
-                        appBinding.music.isPlaying.wrappedValue = false
-                        appBinding.music.playButtonIsDisabled.wrappedValue = true
-                    }
+                    sheetOnDismiss()
                    }
                    , content: {
                     MusicSettingsView(appBinding: appBinding)
                    })
             
             .onAppear {
-                appBinding.music.showActivityIndicator.wrappedValue = true
+                onAppearCheak()
                 
-                
-                DispatchQueue.global(qos: .userInteractive).async {
-                    if AppleMusicAPI.share.getUserToken() == nil {
-                        print("Подписки Apple music - нет")
-
-                        DispatchQueue.main.async {
-                            UIApplication.shared.windows.first?.rootViewController?.showAlert(with: NSLocalizedString("Внимание", comment: ""), and: NSLocalizedString("Ваше устройство не имеет подписки Apple Music", comment: ""), style: .alert) {
-                                presentationMode.wrappedValue.dismiss()
-                                appBinding.music.showActivityIndicator.wrappedValue = false
-                            }
-                        }
-                        
-                    } else {
-                        print("Подписка Apple music есть")
-                        
-                        if appBinding.music.listMusic.wrappedValue.isEmpty {
-                            musicPlayer.pause()
-                            appBinding.music.isPlaying.wrappedValue = false
-                            appBinding.music.playButtonIsDisabled.wrappedValue = true
-                        } else {
-                            musicPlayer.setQueue(with: [appBinding.music.resultMusic.wrappedValue.id ?? ""])
-                        }
-                        
-                        getMusicFile(state: appBinding)
-
-                        if self.musicPlayer.playbackState == .playing {
-                            appBinding.music.isPlaying.wrappedValue = true
-                        } else {
-                            appBinding.music.isPlaying.wrappedValue = false
-                        }
-                        
-                        appBinding.music.showActivityIndicator.wrappedValue = false
-                    }
-                    
-                }
             }
         }
     }
@@ -312,6 +272,59 @@ private extension MusicView {
     private func saveFilmsToUserDefaults(state: Binding<AppState.AppData>) {
         injected.interactors.musicInteractor
             .saveFilmsToUserDefaults(state: state)
+    }
+    
+    private func sheetOnDismiss() {
+        if appBinding.music.listMusic.wrappedValue.isEmpty {
+            getMusicFile(state: appBinding)
+            musicPlayer.pause()
+            appBinding.music.isPlaying.wrappedValue = false
+            appBinding.music.playButtonIsDisabled.wrappedValue = true
+        }
+    }
+    
+    private func onAppearCheak() {
+        appBinding.music.showActivityIndicator.wrappedValue = true
+        DispatchQueue.global(qos: .userInteractive).async {
+            if AppleMusicAPI.share.getUserToken() == nil {
+                noSubscriptionForAppleMusic()
+            } else {
+                subscriptionForAppleMusic()
+            }
+        }
+    }
+    
+    private func subscriptionForAppleMusic() {
+        print("Подписка Apple music есть")
+        
+        if appBinding.music.listMusic.wrappedValue.isEmpty {
+            musicPlayer.pause()
+            appBinding.music.isPlaying.wrappedValue = false
+            appBinding.music.playButtonIsDisabled.wrappedValue = true
+        } else {
+            musicPlayer.setQueue(with: [appBinding.music.resultMusic.wrappedValue.id ?? ""])
+        }
+        
+        getMusicFile(state: appBinding)
+        
+        if self.musicPlayer.playbackState == .playing {
+            appBinding.music.isPlaying.wrappedValue = true
+        } else {
+            appBinding.music.isPlaying.wrappedValue = false
+        }
+        
+        appBinding.music.showActivityIndicator.wrappedValue = false
+    }
+    
+    private func noSubscriptionForAppleMusic() {
+        print("Подписки Apple music - нет")
+        
+        DispatchQueue.main.async {
+            UIApplication.shared.windows.first?.rootViewController?.showAlert(with: NSLocalizedString("Внимание", comment: ""), and: NSLocalizedString("Ваше устройство не имеет подписки Apple Music", comment: ""), style: .alert) {
+                presentationMode.wrappedValue.dismiss()
+                appBinding.music.showActivityIndicator.wrappedValue = false
+            }
+        }
     }
 }
 

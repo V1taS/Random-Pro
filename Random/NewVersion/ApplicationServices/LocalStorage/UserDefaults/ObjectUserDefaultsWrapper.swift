948e1ed621e0d52474d8da57c823df2fd600653b
Sosin Vitalii <375693@mail.ru>
2022-05-05 22:36:17 +0300

    Add UserDefaultsWrapper

---
+++ Random/NewVersion/ApplicationServices/LocalStorage/UserDefaults/ObjectUserDefaultsWrapper.swift
@@ -0,0 +1,45 @@
+//
+//  ObjectUserDefaultsWrapper.swift
+//  Random Pro
+//
+//  Created by Vitalii Sosin on 05.05.2022.
+//  Copyright © 2022 Sosin.bet. All rights reserved.
+//
+
+import Foundation
+
+/// Враперр для стандартных Object. Например: `Int, Double, String, [Float]...`
+///
+/// Пример использования
+///
+/// `@ObjectUserDefaultsWrapper(key: "Уникальный ключ")private var object: T`
+///
+
+/// Сгруппировали нужные протоколы
+typealias ObjectUserDefaultsProtocol = Equatable & Encodable & Decodable
+
+@propertyWrapper
+struct ObjectUserDefaultsWrapper<T: ObjectUserDefaultsProtocol> {
+    
+    // MARK: - Internal property
+    
+    var wrappedValue: T? {
+        get {
+            let object: T? = UserDefaultsWrapper.object(for: key)
+            return object
+        }
+        set {
+            UserDefaultsWrapper.set(newValue, for: key)
+        }
+    }
+    
+    // MARK: - Private property
+    
+    private let key: String
+    
+    // MARK: - Initialization
+    
+    init(key: String) {
+        self.key = key
+    }
+}

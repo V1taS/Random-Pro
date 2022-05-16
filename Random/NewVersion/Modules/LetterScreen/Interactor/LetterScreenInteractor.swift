30879d6f759f668257c55d6f81f652a7c0d5e32f
Sosina Tatyana <fedorova_t95@mail.ru>
2022-05-16 17:35:34 +0300

    Add new screen "LetterScreen" and settings logic

---
+++ Random/NewVersion/Modules/LetterScreen/Interactor/LetterScreenInteractor.swift
@@ -0,0 +1,84 @@
+//
+//  LetterScreenInteractor.swift
+//  Random Pro
+//
+//  Created by Tatiana Sosina on 16.05.2022.
+//  Copyright © 2022 Sosin.bet. All rights reserved.
+//
+
+import UIKit
+
+protocol LetterScreenInteractorOutput: AnyObject {
+    
+    /// Данные були получены
+    /// - Parameter result: результат генерации
+    func didRecive(result: String?)
+    
+    /// Возвращает перевернутый список результатов
+    /// - Parameter listResult: список генераций
+    func didRecive(listResult: [String])
+}
+
+protocol LetterScreenInteractorInput: AnyObject {
+    
+    /// Получить данные
+    func getContent()
+    
+    /// Сoздать новые данные русских букв
+    func generateContentRusLetter()
+    
+    /// Создать новые данные английских букв
+    func generateContentEngLetter()
+}
+
+final class LetterScreenInteractor: LetterScreenInteractorInput {
+    
+    // MARK: - Internal property
+    
+    weak var output: LetterScreenInteractorOutput?
+    
+    // MARK: - Private property
+    
+    private var result = "?"
+    private var listResult: [String] = []
+    
+    // MARK: - Internal func
+    
+    func getContent() {
+        output?.didRecive(result: result)
+        output?.didRecive(listResult: listResult)
+    }
+    
+    func generateContentRusLetter() {
+        let russianLetter = Appearance().listRussionLetter.shuffled().first ?? ""
+        listResult.append(russianLetter)
+        result = russianLetter
+        
+        output?.didRecive(listResult: listResult)
+        output?.didRecive(result: result)
+    }
+    
+    func generateContentEngLetter() {
+        let englishLetter = Appearance().listEnglishLetter.shuffled().first ?? ""
+        listResult.append(englishLetter)
+        result = englishLetter
+
+        output?.didRecive(result: result)
+        output?.didRecive(listResult: listResult)
+    }
+}
+
+// MARK: - private LetterScreenInteractor
+
+private extension LetterScreenInteractor {
+    struct Appearance {
+        let listRussionLetter = [
+            "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т",
+            "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"
+        ]
+        let listEnglishLetter = [
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"
+        ]
+    }
+}
---
+++ Random/NewVersion/Modules/LetterScreen/Presenter/LetterScreenModule.swift
@@ -0,0 +1,111 @@
+//
+//  LetterScreenModule.swift
+//  Random Pro
+//
+//  Created by Tatiana Sosina on 16.05.2022.
+//  Copyright © 2022 Sosin.bet. All rights reserved.
+//
+
+import UIKit
+
+protocol LetterScreenModuleOutput: AnyObject {
+    
+}
+
+protocol LetterScreenModuleInput: AnyObject {
+    
+    /// События которые отправляем из `текущего модуля` в  `другой модуль`
+    var moduleOutput: LetterScreenModuleOutput? { get set }
+}
+
+typealias LetterScreenModule = UIViewController & LetterScreenModuleInput
+
+final class LetterScreenViewController: LetterScreenModule {
+    
+    // MARK: - Internal property
+    
+    weak var moduleOutput: LetterScreenModuleOutput?
+    
+    // MARK: - Private property
+    
+    private let moduleView: LetterScreenViewProtocol
+    private let interactor: LetterScreenInteractorInput
+    private let factory: LetterScreenFactoryInput
+    
+    // MARK: - Initialization
+    
+    init(moduleView: LetterScreenViewProtocol,
+         interactor: LetterScreenInteractorInput,
+         factory: LetterScreenFactoryInput) {
+        self.moduleView = moduleView
+        self.interactor = interactor
+        self.factory = factory
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Internal func
+    
+    override func loadView() {
+        super.loadView()
+        view = moduleView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar()
+        interactor.getContent()
+    }
+        
+    // MARK: - Private func
+    
+    private func navigationBar() {
+        let appearance = Appearance()
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        title = appearance.title
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: appearance.settingsButtonIcon,
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(settingButtonAction))
+    }
+    
+    @objc private func settingButtonAction() {
+    }
+}
+
+extension LetterScreenViewController: LetterScreenViewOutput {
+    func generateEngButtonAction() {
+        interactor.generateContentEngLetter()
+    }
+    
+    func generateRusButtonAction() {
+        interactor.generateContentRusLetter()
+    }
+}
+
+extension LetterScreenViewController: LetterScreenInteractorOutput {
+    func didRecive(result: String?) {
+        moduleView.set(result: result)
+    }
+    
+    func didRecive(listResult: [String]) {
+        factory.resive(listResult: listResult)
+    }
+}
+
+extension LetterScreenViewController: LetterScreenFactoryOutput {
+    func didReverse(listResult: [String]) {
+        moduleView.set(listResult: listResult)
+    }
+}
+
+extension LetterScreenViewController {
+    struct Appearance {
+        let title = "Буква"
+        let settingsButtonIcon = UIImage(systemName: "gear")
+    }
+}

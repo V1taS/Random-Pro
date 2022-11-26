9d080a1f2b0cff2870d4911c340ca3681dd59e84
Sosin Vitalii <sosinvitalii@icloud.com>
2022-11-26 18:10:23 +0300

    Switch off metrick

--- Random Pro/System/AppDelegate.swift
+++ Random Pro/System/AppDelegate.swift
@@ -15,9 +15,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Initializing the AppMetrica SDK.
-    if let configuration = YMMYandexMetricaConfiguration.init(apiKey: "b4921e71-faf2-4bd3-8bea-e033a76457ae") {
-      YMMYandexMetrica.activate(with: configuration)
-    }
+//    if let configuration = YMMYandexMetricaConfiguration.init(apiKey: "b4921e71-faf2-4bd3-8bea-e033a76457ae") {
+//      YMMYandexMetrica.activate(with: configuration)
+//    }
     return true
   }
   

a2eca8d975a0f73ec3b41df2a2eb48918eab837a
V1taS <SosinVitalii@iCloud.com>
2023-05-03 16:38:09 +0200

    Added dependency versioning

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -4,16 +4,16 @@ let dependencies = Dependencies(
   swiftPackageManager: SwiftPackageManagerDependencies(
     [
       .remote(url: "https://github.com/V1taS/RandomUIKit.git",
-              requirement: .branch("master")),
+              requirement: .exact("1.1")),
       .remote(url: "https://github.com/V1taS/RandomNetwork.git",
-              requirement: .branch("master")),
+              requirement: .exact("1.0")),
       .remote(url: "https://github.com/V1taS/Notifications",
-              requirement: .branch("master")),
+              requirement: .exact("1.0")),
       .remote(url: "https://github.com/evgenyneu/keychain-swift.git",
               requirement: .branch("master")),
       
-      .remote(url: "https://github.com/yandexmobile/metrica-sdk-ios",
-              requirement: .upToNextMajor(from: "4.4.0")),
+        .remote(url: "https://github.com/yandexmobile/metrica-sdk-ios",
+                requirement: .upToNextMajor(from: "4.4.0")),
       .remote(url: "https://github.com/yandexmobile/metrica-push-sdk-ios",
               requirement: .upToNextMajor(from: "1.0.0")),
       .remote(url: "https://github.com/firebase/firebase-ios-sdk.git",

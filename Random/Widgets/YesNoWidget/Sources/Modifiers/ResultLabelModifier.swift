09faa0764ac8b6dc75e1495797e4dc8b6c52be2a
Ivlev Nikita <evilevnik@gmail.com>
2023-05-09 14:13:24 +0600

    Add modifiers back

---
+++ Random/Widgets/YesNoWidget/Sources/Modifiers/ResultLabelModifier.swift
@@ -0,0 +1,28 @@
+//
+//  ResultLabelModifier.swift
+//  Random
+//
+//  Created by Nikita Ivlev on 7/5/23.
+//  Copyright © 2023 SosinVitalii.com. All rights reserved.
+//
+
+import SwiftUI
+import RandomUIKit
+
+// Структура копирует стиль resultLabel
+// Для копировния необходимо просто подписать кнопку под модификатор .modifier(ResultLabelModifier(fontSize: *))
+struct ResultLabelModifier: ViewModifier {
+  
+  var fontSize: Int
+  
+  init(fontSize: Int) {
+    self.fontSize = fontSize
+  }
+  
+  func body(content: Content) -> some View {
+    content
+      .font(.system(size: CGFloat(fontSize), weight: .bold))
+      .foregroundColor(Color(RandomColor.darkAndLightTheme.primaryGray))
+      .multilineTextAlignment(.center)
+  }
+}

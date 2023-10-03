51b155e23d47d0df2cbea2a952bdc5d2e5a60d61
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-10-03 16:53:54 +0500

    Add coins to CoinStyleScreenModule

--- Random/App/Sources/Modules/MainScreen/CoinModules/CoinStyleSelectionScreenModule/Models/CoinStyleSelectionScreenModel.swift
+++ Random/App/Sources/Modules/MainScreen/CoinModules/CoinStyleSelectionScreenModule/Models/CoinStyleSelectionScreenModel.swift
@@ -49,26 +49,76 @@ struct CoinStyleSelectionScreenModel: UserDefaultsCodable {
     /// Стиль по дефолту
     case defaultStyle
 
-    /// Доллар
-    case presidentDollar
+    /// Австралийский Кенгуру 2 доллара
+    case australianKangaroo
 
-    /// Рубль
-    case ruble
+    /// Кипрские 5 евро
+    case cyprus5Uero
 
     /// Итальянский евро
-    case euroItalian
+    case italianEuro
+
+    /// Итальянский евро с медведем
+    case italianEuroBear
+
+    /// Грузинский лари День Земли
+    case georgianLari
+
+    /// Германский Евро со шмелем
+    case germanyBumblebeeEuro
+
+    /// Германский Евро Ганс
+    case germanyHancEuro
+
+    /// 10 Рублей
+    case russian10Rub
+
+    /// Рубль Антошка
+    case russianAntoshka
+
+    /// Испанский евро Виктория
+    case spanishVictoriaEuro
+
+    /// Испанский евро Торо
+    case spanishToroEuro
+
+    /// США Вашингтон доллар
+    case usaPresidentDollar
+
+    /// США Хаббл доллар
+    case usaHubbleDollar
 
     /// Данные стиля для сторон монетки
     var coinSidesName: (eagle: String, tails: String) {
       switch self {
       case .defaultStyle:
         return (eagle: "coin_eagle", tails: "coin_tails")
-      case .presidentDollar:
-        return (eagle: "PresidentDollarEagle", tails: "PresidentDollarTails")
-      case .ruble:
-        return (eagle: "RubleEagle", tails: "RubleTails")
-      case .euroItalian:
-        return (eagle: "EuroItaliaEagle", tails: "EuroItaliaTails")
+      case .australianKangaroo:
+        return (eagle: "AustralianKangarooEagle", tails: "AustralianKangarooTails")
+      case .cyprus5Uero:
+        return (eagle: "CyprusEagle", tails: "CyprusTails")
+      case .italianEuro:
+        return (eagle: "ItalianEuroEagle", tails: "ItalianEuroTails")
+      case .italianEuroBear:
+        return (eagle: "ItalianEuroBearEagle", tails: "ItalianEuroBearTails")
+      case .georgianLari:
+        return (eagle: "GeorgianLariEagle", tails: "GeorgianLariTails")
+      case .germanyBumblebeeEuro:
+        return (eagle: "GermanyBumblebeeEuroEagle", tails: "GermanyBumblebeeEuroTails")
+      case .germanyHancEuro:
+        return (eagle: "GermanyHancEuroEagle", tails: "GermanyHancEuroTails")
+      case .russian10Rub:
+        return (eagle: "Russian10RubEagle", tails: "Russian10RubTails")
+      case .russianAntoshka:
+        return (eagle: "RussianAntoshkaEagle", tails: "RussianAntoshkaTails")
+      case .spanishVictoriaEuro:
+        return (eagle: "SpanishVictoriaEuroEagle", tails: "SpanishVictoriaEuroTails")
+      case .spanishToroEuro:
+        return (eagle: "SpanishToroEuroEagle", tails: "SpanishToroEuroTails")
+      case .usaPresidentDollar:
+        return (eagle: "USAPresidentDollarEagle", tails: "USAPresidentDollarTails")
+      case .usaHubbleDollar:
+        return (eagle: "USAHubbleEagle", tails: "USAHubbleTails")
       }
     }
   }
